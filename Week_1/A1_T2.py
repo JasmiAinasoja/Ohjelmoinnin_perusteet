55dcb9132482056e805fd76ff807febbd4c31e3b
Jasmi Ainasoja <jasmi.ainasoja@gmail.com>
2025-09-19 19:20:29 +0300

    Muutettu kansion nimi

---
+++ Week_1/A1_T2.py
@@ -0,0 +1,15 @@
+# Make Python program to do the following:
+# 1. Assign variable with string value: "John"
+Name1 = "John"
+# 2. Print the variable
+print(Name1)
+# 3. Assign second variable with another string value: "Harry"
+Name2 = "Harry"
+# 4. Print the second variable
+print(Name2)
+# 5. Make the program to print both names on the screen at the same row separated by space-character
+print(Name1, Name2)
+# 6. Make the program to print a string "is eating ice cream with" between the previously created variables.
+print(Name1, "is eating ice cream with", Name2)
+# 7. Make the program to print one more row to the screen which outputs following "Tom and Harry are friends". Use the existing variables to do so.
+print(Name1, "and", Name2, "are friends")
